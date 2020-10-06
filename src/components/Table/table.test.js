42c07ecafd897571284b8b107ddf0773142f50d2
Uzor Efedigbue <cefedigbuej@gmail.com>
2020-10-06 18:37:14 +0100

    stop point, included enzyme in app dependency

--- src/components/Table/table.test.js
+++ src/components/Table/table.test.js
@@ -4,23 +4,25 @@ import Table from './table';
 import renderer from 'react-test-renderer'
 
 describe('Table', () => {
-  const list = [
+  const props = {
+    list: [
     { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
-    { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' }]
+    { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' }],
+  }
 
     const dummyFunc = () => 1
 
   it('renders', () => {
     const div = document.createElement('div')
-    ReactDOM.render(<Table  list={list} onDismiss={dummyFunc} />, div)
+    ReactDOM.render(<Table  list={props.list} onDismiss={dummyFunc} />, div)
   })
 
   test('snapshots', () => {
 
     const component = renderer.create(
-      <Table  list={list} onDismiss={dummyFunc} />
+      <Table  list={props.list} onDismiss={dummyFunc} />
     )
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
-  });
+  })
 })
